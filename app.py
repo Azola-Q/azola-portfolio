89af6ca19fe8794200876a2103fe481cdcf6f650
Azola-Q <159591312+Azola-Q@users.noreply.github.com>
2025-09-19 11:49:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
 from flask_mail import Mail, Message
 import os
 
