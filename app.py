b99dd4d3c6164ce333fb134f90423c8f8aa4756f
Azola-Q <159591312+Azola-Q@users.noreply.github.com>
2025-09-19 11:13:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'azolaqakaqu@gmail.com'      # <-- your Gmail address
 app.config['MAIL_PASSWORD'] = 'xnlnwaqceukdpoon'        # <-- Gmail App Password
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 mail = Mail(app)
 
